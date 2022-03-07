af01966267c0a295ac119c02936907faecab8048
Jonathan <76862328+J-o-nathan@users.noreply.github.com>
2022-03-07 14:01:35 +1100

    Delete src/tests directory

--- src/tests/components/LessonDashboardPage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import LessonDashboardPage from '../../components/LessonDashboardPage';
-
-test('should render LessonDashboardPage correctly', () => {
-  const wrapper = shallow(<LessonDashboardPage />);
-  expect(wrapper).toMatchSnapshot();
-});
